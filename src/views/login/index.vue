f031f937af7a730c6d2f49727e9349a60d4a56c2
vickiwu <794431483@qq.com>
2020-09-24 13:42:13 +0800

    登录页

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -12,83 +12,130 @@
 
         <div class="title-container">
           <el-row>
-            <el-col :span="12" :class="isActive&& 'active'" @click.native="isActive=!isActive">账号密码登录</el-col>
-            <el-col :span="12" :class="!isActive&& 'active'" @click.native="isActive=!isActive">验证码登录</el-col>
+            <el-col
+              :span="12"
+              :class="isActive ==='account'&& 'active'"
+              @click.native="isActive='account'"
+            >账号密码登录</el-col>
+            <el-col
+              :span="12"
+              :class="isActive==='phone'&& 'active'"
+              @click.native="isActive='phone'"
+            >验证码登录</el-col>
           </el-row>
-          <div class="active-line" />
+          <div
+            class="active-line"
+            :class="isActive==='phone'?'animation-left':'animation-right'"
+          />
 
         </div>
 
-        <el-form-item prop="username">
-          <span class="svg-container">
-            <svg-icon icon-class="user" />
-          </span>
-          <el-input
-            ref="username"
-            v-model="loginForm.username"
-            placeholder="请输入用户名"
-            name="username"
-            type="text"
-            tabindex="1"
-            auto-complete="on"
-          />
-        </el-form-item>
+        <div v-if="isActive==='account'">
+          <el-form-item prop="username">
+            <span class="svg-container">
+              <svg-icon icon-class="user" />
+            </span>
+            <el-input
+              ref="username"
+              v-model="loginForm.username"
+              placeholder="请输入用户名"
+              name="username"
+              type="text"
+              tabindex="1"
+              auto-complete="on"
+            />
+          </el-form-item>
 
-        <el-form-item prop="password">
-          <span class="svg-container">
-            <svg-icon icon-class="password" />
-          </span>
-          <el-input
-            :key="passwordType"
-            ref="password"
-            v-model="loginForm.password"
-            :type="passwordType"
-            placeholder="请输入密码"
-            name="password"
-            tabindex="2"
-            auto-complete="on"
-            @keyup.enter.native="handleLogin"
-          />
-          <span
-            class="show-pwd"
-            @click="showPwd"
-          >
-            <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
-          </span>
-        </el-form-item>
-        <el-row>
-          <el-col :span="18">
-            <el-form-item prop="validate">
-              <span class="svg-container">
-                <svg-icon icon-class="yzm" />
-              </span>
-              <el-input
-                ref="validate"
-                v-model="loginForm.validate"
-                placeholder="请输入验证码"
-                name="validate"
-                type="text"
-                tabindex="1"
-                auto-complete="on"
-              />
+          <el-form-item prop="password">
+            <span class="svg-container">
+              <svg-icon icon-class="password" />
+            </span>
+            <el-input
+              :key="passwordType"
+              ref="password"
+              v-model="loginForm.password"
+              :type="passwordType"
+              placeholder="请输入密码"
+              name="password"
+              tabindex="2"
+              auto-complete="on"
+              @keyup.enter.native="handleLogin"
+            />
+            <span
+              class="show-pwd"
+              @click="showPwd"
+            >
+              <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
+            </span>
+          </el-form-item>
+          <el-row>
+            <el-col :span="18">
+              <el-form-item prop="validate">
+                <span class="svg-container">
+                  <svg-icon icon-class="yzm" />
+                </span>
+                <el-input
+                  ref="validate"
+                  v-model="loginForm.validate"
+                  placeholder="请输入验证码"
+                  name="validate"
+                  type="text"
+                  tabindex="1"
+                  auto-complete="on"
+                />
 
-            </el-form-item>
-          </el-col>
-          <el-col :span="6">
-            <div class="valid-img">
-              <img
-                src="data:image/jpg;base64,iVBORw0KGgoAAAANSUhEUgAAADwAAAAUCAIAAABeYcl+AAAA2UlEQVR42u3W0QnDIBAGYCfJDu0YWaRbdLEOUOhT37JEdmiFgx+5/86obQ0Nig/JSciHnqfh9YctDPTWz0LYAX1/ntF59Hp6mF24sS3TLXb1lQTT+GVepSOISDU6FZtuDy1u+DbRUCp0y0wrKLtTH2Y378arN9ksrkB7KWGihcsrUI6Oz13R5m5rQMOtxC1oL6GRCflF2AFtbkQkQy2aob9Cl29Ebyg/u0r85Zw+OFrFS9Bc8jrVaa53tWhVttvRn5yI+VOQE4PPyK53j3K0VzHS+LiaDvTR0G8iO3BjUHwybgAAAABJRU5ErkJggg=="
-                alt="验证码"
-              >
+              </el-form-item>
+            </el-col>
+            <el-col :span="6">
+              <div class="valid-img">
+                <img
+                  src="data:image/jpg;base64,iVBORw0KGgoAAAANSUhEUgAAADwAAAAUCAIAAABeYcl+AAAA2UlEQVR42u3W0QnDIBAGYCfJDu0YWaRbdLEOUOhT37JEdmiFgx+5/86obQ0Nig/JSciHnqfh9YctDPTWz0LYAX1/ntF59Hp6mF24sS3TLXb1lQTT+GVepSOISDU6FZtuDy1u+DbRUCp0y0wrKLtTH2Y378arN9ksrkB7KWGihcsrUI6Oz13R5m5rQMOtxC1oL6GRCflF2AFtbkQkQy2aob9Cl29Ebyg/u0r85Zw+OFrFS9Bc8jrVaa53tWhVttvRn5yI+VOQE4PPyK53j3K0VzHS+LiaDvTR0G8iO3BjUHwybgAAAABJRU5ErkJggg=="
+                  alt="验证码"
+                >
 
-            </div>
-          </el-col>
-        </el-row>
+              </div>
+            </el-col>
+          </el-row>
 
-        <div class="remember-pwd">
-          <el-checkbox v-model="checked">记住密码</el-checkbox>
+          <div class="remember-pwd">
+            <el-checkbox v-model="checked">记住密码</el-checkbox>
+          </div>
         </div>
 
+        <div v-if="isActive==='phone'">
+          <el-form-item prop="username">
+            <span class="svg-container">
+              <svg-icon icon-class="user" />
+            </span>
+            <el-input
+              ref="username"
+              v-model="loginForm.username"
+              placeholder="请输入手机号码"
+              name="username"
+              type="text"
+              tabindex="1"
+              auto-complete="on"
+            />
+          </el-form-item>
+
+          <el-form-item prop="username">
+            <span class="svg-container">
+              <svg-icon icon-class="yzm" />
+            </span>
+            <el-input
+              ref="username"
+              v-model="loginForm.username"
+              placeholder="请输入验证码"
+              name="username"
+              type="text"
+              tabindex="1"
+            />
+            <span class="send-yzm">
+              发送验证码
+            </span>
+          </el-form-item>
+
+        </div>
         <el-button
           :loading="loading"
           type="primary"
@@ -131,7 +178,7 @@ export default {
     }
     return {
       checked: true,
-      isActive: true,
+      isActive: 'account',
       loginForm: {
         username: 'admin',
         password: 'admin',
@@ -243,15 +290,18 @@ $light_gray: #333333;
 .login-container {
   min-height: 100%;
   width: 100%;
-  background-color: $bg;
+  // background-color: $bg;
+  background: #d2e8f6 url('~@/assets/bg.png') center fixed no-repeat;
+  background-size: cover;
   overflow: hidden;
   display: flex;
   align-items: center;
   justify-content: center;
 
   .login-card {
-    width: 480px;
+    width: 440px;
     max-width: 100%;
+    height: 380px;
     .login-form {
       padding: 5px 0px;
       margin: 0 auto;
@@ -264,6 +314,14 @@ $light_gray: #333333;
         display: flex;
         align-items: center;
         margin-left: 10px;
+        cursor: pointer;
+      }
+      .send-yzm {
+        position: absolute;
+        right: 10px;
+        top: 7px;
+        font-size: 13px;
+        color: #409EFF;
         cursor: pointer;
       }
     }
@@ -292,18 +350,18 @@ $light_gray: #333333;
   .title-container {
     height: 40px;
     padding: 5px 20px;
+    margin-bottom: 15px;
     // line-height: 40px;
     .active-line {
-        width: 20px;
-        height: 3px;
-        background:#66b1ff;
-        margin: 0 10px;
-      }
+      width: 80px;
+      height: 3px;
+      background: #66b1ff;
+      margin: 3px 10px 0 10px;
+    }
     .el-row {
-
       .el-col {
-        &.active{
-          color:#66b1ff;
+        &.active {
+          color: #66b1ff;
         }
         cursor: pointer;
         margin-bottom: 5px;
@@ -319,6 +377,55 @@ $light_gray: #333333;
     color: $dark_gray;
     cursor: pointer;
     user-select: none;
+  }
+}
+.animation-right {
+  -webkit-animation: fadeInRight 0.5s 0.1s ease both;
+  -moz-animation: fadeInRight 0.5s 0.1s ease both;
+}
+.animation-left {
+  -webkit-animation: fadeInLeft 0.5s 0.1s ease both;
+  -moz-animation: fadeInLeft 0.5s 0.1s ease both;
+}
+@-webkit-keyframes fadeInLeft {
+  0% {
+    opacity: 0;
+    -webkit-transform: translateX(0px);
+  }
+  100% {
+    opacity: 1;
+    width: 65px;
+    -webkit-transform: translateX(179px);
+  }
+}
+@-moz-keyframes fadeInLeft {
+  0% {
+    opacity: 0;
+    -moz-transform: translateX(0px);
+  }
+  100% {
+    opacity: 1;
+    -moz-transform: translateX(179px);
+  }
+}
+@-webkit-keyframes fadeInRight {
+  0% {
+    opacity: 0;
+    -webkit-transform: translateX(179px);
+  }
+  100% {
+    opacity: 1;
+    -webkit-transform: translateX(0);
+  }
+}
+@-moz-keyframes fadeInRight {
+  0% {
+    opacity: 0;
+    -moz-transform: translateX(179px);
+  }
+  100% {
+    opacity: 1;
+    -moz-transform: translateX(0);
   }
 }
 </style>
